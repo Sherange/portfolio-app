0b2f58f3fa9e9214e50dbd8c1d152fe63fa20a41
sherange <sherangef@gmail.com>
2021-09-13 18:06:28 +0530

    side bar and my info ui

---
+++ lib/components/side_menu.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'my_info.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [MyInfo()],
+      ),
+    );
+  }
+}
