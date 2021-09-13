c17816dfc8d253e5078a97936bb7ef3ad00f2708
sherange <sherangef@gmail.com>
2021-09-13 16:51:47 +0530

    data models

---
+++ lib/models/projects.dart
@@ -0,0 +1,24 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List<Project> projects = [
+  Project(
+      title: "EFL Terminal",
+      description:
+          "EFL terminal application is a mobile app that supports 3PL logistic services to automate their business workflows. Sheranga works as a lead mobile developer for this project. The application was developed for the Android mobile operating system using react native technology."),
+  Project(
+      title: "SP Tools",
+      description:
+          "SP tool is the e-commerce mobile application that connects the Shopify ecommerce website using middleware API. Application build for both IOS and Android platforms using react native framework"),
+  Project(
+      title: "Ze Power",
+      description:
+          "Ze power is a mobile application that users can rent from the power banks just like renting the bikes using a mobile application. The application is built for both IOS and Android platforms using react native framework."),
+  Project(
+      title: "The Horsemap",
+      description:
+          "Horsemap is a mobile application that facilitates users to publish their advertisements regarding the horses or pony The app facilitates geo locate the horse for sale/grooms etc.The application built for both IOS and Android platforms using react native framework."),
+];
---
+++ lib/models/recommendations.dart
@@ -0,0 +1,17 @@
+class Recommendation {
+  final String? name, source, text;
+
+  Recommendation({this.name, this.source, this.text});
+}
+
+final List<Recommendation> recommendations = [
+  Recommendation(
+      name: "Kolapo Obenewa",
+      source: "Linkedin",
+      text:
+          "I have the pleasure of working with Anwar on two flutter projects and waht strikes me the most about"),
+  Recommendation(
+      name: "Reza Shahbazi",
+      source: "Linkedin",
+      text: "Abu is greated asset for any organization.")
+];
