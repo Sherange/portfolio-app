47b53ff4239957c3916ec858d640915e464f7047
sherange <sherangef@gmail.com>
2021-09-13 16:32:54 +0530

    app constants

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFFFc107);
+const secondryColor = Color(0xFF242430);
+const darkColor = Color(0xFF191923);
+const bodyTextColor = Color(0xFF8B888D);
+const bgColor = Color(0xFF1E1E28);
+
+const defaultPadding = 20.0;
+const defaulDuration = Duration(seconds: 1); //for animations
+const maxWidth = 1440.0; //max width of our web 
+ 
