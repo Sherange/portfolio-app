0a5a4d2dbdd6fa206b5ac5e0e2df3c4191294f02
sherange <sherangef@gmail.com>
2021-09-14 12:38:01 +0530

    side menu footer changes

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../../../constants.dart';
+
 import 'area_info.dart';
 import 'codeing.dart';
 import 'knowledge.dart';
@@ -41,6 +43,61 @@ class SideMenu extends StatelessWidget {
 
                   //knowledge section
                   Knowledge(),
+
+                  Divider(),
+                  SizedBox(height: defaultPadding / 2),
+
+                  TextButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: defaultPadding),
+                      child: FittedBox(
+                        child: Row(
+                          children: [
+                            Text(
+                              "DOWNLOAD CV",
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyText1!
+                                      .color),
+                            ),
+                            SizedBox(
+                              width: defaultPadding / 2,
+                            ),
+                            SvgPicture.asset('assets/icons/download.svg'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: defaultPadding),
+                    color: Color(0xFF24242E),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset('assets/icons/linkedin.svg'),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset('assets/icons/twitter.svg'),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset('assets/icons/github.svg'),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: SvgPicture.asset('assets/icons/dribble.svg'),
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
